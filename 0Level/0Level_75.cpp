19527980dfbef5ce3690943c2aab103a5888295c
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2023-12-08 15:27:20 +0900

    2023_12_8(mac1)

---
+++ 0Level/0Level_75.cpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <vector>
+#include <iostream>
+// 삼각형의 완성 조건(1)
+using namespace std;
+
+int solution(vector<int> sides)
+{
+    int answer = 0;
+    vector<int> another;
+    int max = 0;
+
+    for (auto a : sides)
+    {
+        if (max < a)
+        {
+            max = a;
+        }
+    }
+
+    for (auto b : sides)
+    {
+        if (b != max)
+        {
+            another.push_back(b);
+        }
+    }
+
+    while (true)
+    {
+        if (another.size() < 2)
+        {
+            another.push_back(max);
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    if (max < another[0] + another[1])
+    {
+        answer = 1;
+    }
+    else
+    {
+        answer = 2;
+    }
+    return answer;
+}
