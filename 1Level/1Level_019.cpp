d723704940583d6bd378cd80a6a783a827b45536
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2023-12-31 03:43:00 +0900

    2023_12_31(콜라츠 추측_Use Long Long)

---
+++ 1Level/1Level_019.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+// ÄÝ¶óÃ÷ ÃßÃø
+using namespace std;
+
+int solution(int num) {
+    int answer = 0;
+    int count = 0;
+    bool flag = true;
+
+    long long numcopy = num;
+    while (flag) {
+        if (numcopy == 1) {
+            answer = count;
+            flag = false;
+        } else if ((numcopy % 2) == 0) {
+            numcopy /= 2;
+            cout << numcopy << endl;
+            count++;
+        } else if ((numcopy % 2) != 0) {
+            numcopy = (numcopy * 3) + 1;
+            cout << numcopy << endl;
+            count++;
+        }
+
+        if (count >= 500) {
+            answer = -1;
+            flag = false;
+        }
+    }
+    return answer;
+}
